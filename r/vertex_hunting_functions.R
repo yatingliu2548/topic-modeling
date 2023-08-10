bae564eaff117684ea6d9f584d2fdafbe431e6e2
yatingliu2548 <13189092548@163.com>
2023-08-09 22:15:12 -0500

    adding AA to vertex_hunting algo

--- r/vertex_hunting_functions.R
+++ r/vertex_hunting_functions.R
@@ -125,3 +125,12 @@ vertices_est <- function(R, K0, m){
   }
  
 }
+
+
+ArchetypeA <- function(R,K){
+  library(reticulate)
+  use_condaenv("r-reticulate")
+  source_python("NMf.py")
+  resultfromAA= acc_palm_nmf(X=R,r=K,proj_method='wolfe', m=5, maxiter=1000, c1 =1, c2 = 1, verbose = True, plotit = True, proj_low_dim = False, oracle = False, weights_exact = False, method = 'fista', fixed_max_size=5)
+  return(list(V=resultfromAA$V,Weigh_hat=resultfromAA$weight))
+}
