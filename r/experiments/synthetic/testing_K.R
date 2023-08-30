5007deb72fd5ffacad17f013c1de8b4b16112cf3
donnate <cdonnat@uchicago.edu>
2023-08-30 13:42:43 -0500

    large p

---
+++ r/experiments/synthetic/testing_K.R
@@ -0,0 +1,63 @@
+source("r/experiments/semi_synthetic/synthetic_AP.R")
+source("r/experiments/synthetic/synthetic_dataset.R")
+
+args = commandArgs(trailingOnly=TRUE)
+seed <-  ceiling(as.numeric(args[1]))
+result_file <- args[2]
+K <- ceiling(as.numeric(args[3]))
+#matlab_path <-  args[4]
+alpha_dirichlet <-  as.numeric(args[4])
+n_anchors <-  as.numeric(args[5])
+N <-  as.numeric(args[6])
+n <-  as.numeric(args[7])
+#matlab_path = DEFAULT_MATLAB
+error <- c()
+
+b_zipf <- 2.7
+a_zipf <- 1
+
+for (delta_anchor in 10^(-(1:5))){
+        for (p in c(1000, 5000, 10000, 15000)){
+            if (K <5){
+                VHMethod = "SVS"
+            }else{
+                VHMethod = "SP"
+            }
+            test <- run_synthetic_experiment(n, K, p, alpha=alpha_dirichlet, 
+                                                a_zipf=a_zipf, offset_zipf = b_zipf,
+                                                n_anchors=n_anchors, delta_anchor=delta_anchor, N=N,
+                                                seed=seed, VHMethod=VHMethod,
+                                                data_generation_method=1,
+                                                normalize_counts = TRUE,
+                                                sparse = TRUE)
+            error_temp = test$error
+            error_temp["Khat_huy"]=test$Khat_huy
+            error_temp["Khat_huy_thresh"] = test$Khat_huy_thresh
+            error_temp["Khat_olga"]= test$Khat_olga
+            error_temp["Khat_olga_thresh"] = test$Khat_olga_thresh
+            error_temp["Khat_tracy"]=test$Khat_tracy
+            error_temp["Khat_tracy_thresh"] = test$Khat_tracy_thresh
+            error_temp["N"] = N
+            error_temp["n"] = n
+            error_temp["p"] = p
+            error_temp["seed"] =  seed 
+            error_temp["n_anchors"] =  n_anchors
+            error_temp["delta_anchor"] =  delta_anchor
+            error_temp["a_zipf"] = 1
+            error_temp["offset_zipf"] = b_zipf
+            error_temp["alpha_dirichlet"] = alpha_dirichlet
+            error_temp["exp"] = result_file
+            error_temp["VHMethod"] = VHMethod
+            error_temp["delta_anchor"] = delta_anchor
+            error <- rbind(error,
+                            error_temp)
+            write_csv(error, paste0(getwd(), paste0("/r/experiments/synthetic/results/",paste0(result_file,'_K_' , K), ".csv")))
+
+        }
+        
+}
+        
+
+
+
+
---
+++ r/experiments/synthetic/testing_K.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=array
+#SBATCH --output=r/experiments/synthetic/logs/array_%A_%a.out
+#SBATCH --error=r/experiments/synthetic/logs/array_%A_%a.err
+#SBATCH --array=1-50
+#SBATCH --time=35:00:00
+#SBATCH --partition=caslake
+#SBATCH --ntasks=5
+#SBATCH --mem=20G
+#SBATCH --account=pi-cdonnat
+
+# Print the task id.
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "My SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+# Add lines here to run your computations
+job_id=$SLURM_ARRAY_JOB_ID
+module load gsl
+module load R/4.2.0
+module load matlab
+module load python
+
+MATLAB_PATH="/software/matlab-2023a-el8-x86_64/bin/matlab"
+result_file="testing_K_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+echo "result file is ${result_file}"
+cd $SCRATCH/$USER/topic-modeling/
+Rscript r/experiments/synthetic/testing_K.R $SLURM_ARRAY_TASK_ID $result_file $1 $2 $3 $4 $5
